e55baedae3854b2cb696f329bffe6bf28505c980
Koshik Bharat Mehta <koshikmehta2795@gmail.com>
2024-12-26 18:58:28 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 
 
 const generateToken = (user)=>{
-       return jwt.sign({ email: user.email, id: user._id}, abdbjbdjbsJjbdjadbjasb);
+       return jwt.sign({ email: user.email, id: user._id}, "abdbjbdjbsJjbdjadbjasb");
 }
 
 
