888e72150b2c67efd32f310b51782f9fafae802a
Koshik Bharat Mehta <koshikmehta2795@gmail.com>
2024-12-26 17:54:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
     })
 );
 app.use(flash());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 app.set('view engine', 'ejs');
 
 app.use("/", indexRouter)
