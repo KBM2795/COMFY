b86e4cc62174bbd2c3d947095bd822fe274de34c
Koshik <koshikmehta2795@gmail.com>
2024-12-26 10:04:55 +0530

    Completed app

---
+++ config/mongoose-connection.js
@@ -0,0 +1,13 @@
+const { log } = require('console');
+const mongoose = require('mongoose');
+
+mongoose
+.connect("mongodb://127.0.0.1:27017/COMFY")
+.then(function(){
+     console.log("Connected");
+})
+.catch(function(err){
+    console.log("err");
+})
+
+module.exports = mongoose.connection;
